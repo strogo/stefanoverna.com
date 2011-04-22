dc55bf306f644b03618c62ded34f17afebdce93e
Stefano Verna <stefano.verna@gmail.com>
2011-04-22 17:45:21 +0200

    New posts, added feedburner readers counter

---
+++ content/blog/2011/04/css3-github-buttons.md
@@ -0,0 +1,10 @@
+---
+kind: article
+created_at: 04/20/2011 08:19
+title: CSS3 Github buttons
+external_link: http://nicolasgallagher.com/lab/css3-github-buttons/
+---
+
+Belli, stilizzati con cura e semplicità. Disponibili anche tramite [css3buttons_rails_helpers](https://github.com/thetron/css3buttons_rails_helpers), se siete quei tipi di persone che aggiungono una gemma per degli stili CSS.
+
+*Update:* per parcondicio, ecco i [bottoni stile Facebook](http://nicolasgallagher.com/lab/css3-facebook-buttons/).
--- lib/default.rb
+++ lib/default.rb
@@ -8,13 +8,12 @@ module MyHelper
     articles = sorted_articles.slice(page_index*per_page, per_page)
 
     grouped = articles.group_by do |article|
-      date = article.attributes[:created_at]
-      date = DateTime.parse(date)
-      DateTime.new(date.year, date.month, date.day)
+      date = article.attributes[:created_at].getlocal
+      Time.local(date.year, date.month, date.day)
     end
 
     grouped.keys.sort.reverse.each do |date|
-      yield date, grouped[date].sort_by { |article| DateTime.parse(article.attributes[:created_at]) }
+      yield date, grouped[date].sort_by { |article| article.attributes[:created_at] }.reverse
     end
 
   end
